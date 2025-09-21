89590e35d468f4e58c0e0653198f05b21c353ba2
mitadeon <mitadeon@gmail.com>
2025-09-21 10:12:22 +0700

    Tambahkan .gitignore agar .exe tidak ikut push

--- SD tugas 1.4.cpp
+++ SD tugas 1.4.cpp
@@ -5,11 +5,15 @@ int main()
 {
     {
     double x1, y1, x2, y2;
-    cout << "Masukkan koordinat titik A (x1 y1): ";
-    cin >> x1 >> y1;
-    cout << "Masukkan koordinat titik B (x2 y2): ";
-    cin >> x2 >> y2;
-    
+    cout << "Masukkan koordinat titik A (x1): ";
+    cin >> x1 ;
+    cout << "Masukkan koordinat titik A (y1): ";
+    cin >> y1;
+    cout << "Masukkan koordinat titik B (x2): ";
+    cin >> x2 ;
+    cout << "Masukkan koordinat titik B (y2): ";
+    cin >> y2;
+
     double jarak = sqrt (pow(x2 - x1, 2) + pow (y2 - y1, 2));
     cout << "Jarak antara titik A dan B adalah: " << jarak << endl;
 	}
---
+++ SD tugas 1.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, temp;
+
+    cout << "Masukkan bilangan A: ";
+    cin >> A;
+    cout << "Masukkan bilangan B: ";
+    cin >> B;
+
+    
+    temp = A;
+    A = B;
+    B = temp;
+
+    cout << "Setelah ditukar:" << endl;
+    cout << "Nilai A = " << A << endl;
+    cout << "Nilai B = " << B << endl;
+
+    return 0;
+}
